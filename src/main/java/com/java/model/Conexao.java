48cd234449ad3eb6f5b8f04b3e0db3f23bbf7608
Nicolas Skopek <nicolasskopekubc@hotmail.com>
2024-05-04 02:00:40 -0300

    pastas detalhadas

--- src/main/java/com/java/model/Conexao.java
+++ src/main/java/com/java/model/Conexao.java
@@ -14,8 +14,12 @@ public class Conexao {
         try {
             Class.forName("org.postgresql.Driver");
             conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/nomeDoBanco", "username", "password"); // username, por padrão, é postgres. A senha é aquela que você cadastra assim que baixa o PostgreSQL ou pgAdmin4
-            ResultSet rsCliente = conexao.createStatement().executeQuery("SELECT * FROM cadastrar_livro"); // De exemplo, estamos usando o comando SELECT para exibir todas colunas da tabela 'cadastrar_livro'. Os dados que exibimos foram inseridos direto no pgAdmin4 apenas para teste, mas no trabalho final, deverão ser inseridas pelo código Java.
+
+            //(EXEMPLO)
+            ResultSet rsCliente = conexao.createStatement().executeQuery("SELECT * FROM cadastrar_livro"); // De exemplo, estamos usando o comando SELECT para exibir todas colunas da tabela 'cadastrar_livro'. Os dados que exibimos foram inseridos direto no pgAdmin4 apenas para teste, mas no trabalho final, deverão ser inseridas pelo código Java. EXEMPLO: .createStatement().executeQuery("INSERT INTO nomeDaTabela (coluna1, coluna2) VALUES (value1, value2)"); :)
             while (rsCliente.next()) {
+
+                //(EXEMPLO)
                 System.out.println("Nome: " + rsCliente.getString("nome")); // usamos o While para exibir todos. A condição dentro do While faz com que, assim que um elemento da tabela é exibido (no caso, o nome) ele pula para o próximo, até não haver mais elementos a serem exibidos. 
                 // IMPORTANTE: Baixe as 4 primeiras extensões quando você pesquisa 'Spring' na aba de extensões do VSCode.
                 // Tenha certeza de que a tabela esteja criada no pgAdmin4.
