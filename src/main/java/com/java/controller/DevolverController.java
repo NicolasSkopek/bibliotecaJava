2e5afbc56c57a51196bacc8d8157c79f9adf6380
lucasmri23 <132096100+lucasmri23@users.noreply.github.com>
2024-05-07 02:09:04 -0300

    Ideias para lógica das funções de empréstimo

--- src/main/java/com/java/controller/DevolverController.java
+++ src/main/java/com/java/controller/DevolverController.java
@@ -1,5 +1,7 @@
 package com.java.controller;
 
 public class DevolverController {
-    
+    /*Lógica seria baseada em pedir as informações do livro e dia do emprestimo
+    perguntar a data atual, e informar se foi entregue em dia ou atrasado
+    remover a linha do BD*/
 }
