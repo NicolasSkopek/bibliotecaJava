3a645ddf9a7ff4095c5f76078bc0c52ccd0c47dc
lucasmri23 <132096100+lucasmri23@users.noreply.github.com>
2024-05-06 23:24:31 -0300

    Update Usuario.java

--- src/main/java/com/java/model/Usuario.java
+++ src/main/java/com/java/model/Usuario.java
@@ -1,16 +1,10 @@
 package com.java.model;
 
-public class Usuario extends Entidade{
+public class Usuario{
     String nome;
     String telefone;
 
     public Usuario(String nome,String telefone){
-        this.nome = nome;
-        this.telefone = telefone;
-    }
-
-    public Usuario(int id,String nome,String telefone){
-        super.setId(id);
         this.nome = nome;
         this.telefone = telefone;
     }
