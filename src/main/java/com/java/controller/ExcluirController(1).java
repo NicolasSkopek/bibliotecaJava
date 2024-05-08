5c085fcc1cfbbae9c5402fa1b1459af18eebebab
Nicolas Skopek <nicolasskopekubc@hotmail.com>
2024-05-07 21:31:25 -0300

    emprestimo feito

---
+++ src/main/java/com/java/controller/ExcluirController(1).java
@@ -0,0 +1,46 @@
+package com.java.controller;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.sql.Connection;
+
+import com.java.config.ConexaoBanco;
+import com.java.model.LivroModel;
+import com.java.view.MenuView;
+
+public class ExcluirController {
+    private Scanner scanner;
+    private MenuView menuView;
+
+    public ExcluirController(MenuView menuView){
+        scanner = new Scanner(System.in);
+        this.menuView = menuView;
+    }
+    public void ExcluirLivro(){
+    
+        menuView.mensagem("==== Excluindo Livro ====\n");
+        menuView.mensagem("Digite o Id do livro que deseja Remover da lista:");
+        int remov = scanner.nextInt();
+        try (Connection conexao = ConexaoBanco.obterConexao()) {
+            String retorno = removeBD(conexao,remov);
+            menuView.mensagem(retorno);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public String removeBD(Connection conexao, int remov){
+        try {
+            String sql = "DELETE FROM cadastrar_livro WHERE id_livro = ?";
+            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
+            preparedStatement.setInt(1, remov);
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                return "Livro removido com sucesso!";
+            } else {
+                return "Falha ao remover o livro!";
+            }
+        } catch (SQLException ex) {
+            return "Ocorreu um erro ao remover o livro: " + ex.getMessage();
+        }
+    }
+}
---
+++ src/main/java/com/java/controller/ListarController(1).java
@@ -0,0 +1,45 @@
+package com.java.controller;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+
+import com.java.config.ConexaoBanco;
+import com.java.view.MenuView;
+
+public class ListarController {
+    private MenuView menuView;
+
+    public ListarController(MenuView menuView){
+        this.menuView = menuView;
+    }
+    public void ListarLivros(){
+        menuView.mensagem("===== Livros Cadastrados =====");
+
+        try {
+            Connection conexao = ConexaoBanco.obterConexao();
+
+            Statement declaracao = conexao.createStatement();
+
+            String sql = "SELECT * FROM cadastrar_livro";
+            ResultSet resultado = declaracao.executeQuery(sql);
+
+            // Exibe os dados dos livros
+            while (resultado.next()) {
+                int id = resultado.getInt("id_livro");
+                String titulo = resultado.getString("titulo");
+                String autor = resultado.getString("autor");
+                String editora = resultado.getString("editora");
+                String paginas = resultado.getString("paginas");
+                String genero = resultado.getString("genero");
+
+                menuView.mensagem("\nID: " + id + " | Título: " + titulo + " | Autor: " + autor + " | Editora: " + editora + " | Páginas: " + paginas + " | Genero: " + genero);
+            }
+
+            resultado.close();
+            declaracao.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
